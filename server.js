d896900edbb206ba1fcaad5cae3395e5de6cb3cf
Marc Rooding <admin@webresource.nl>
2014-11-05 15:42:53 +0000

    Back-end server used by the async validator

---
+++ server.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var router = express.Router();
+
+app.use('/api', bodyParser.json());
+app.use('/api', router);
+app.listen(3000);
+
+router.get('/validate/:age', function(req, res) {
+  console.log(req.params);
+  res.json(req.params.age > 17);
+});
