b577afc026815e067ef7c6887c6a157d12438f5f
Marc Rooding <admin@webresource.nl>
2014-11-16 16:40:05 +0100

    Added 2 extra test cases and consistently used toBeTruthy()/toBeFalsy()

--- test/spec/validateMustEqualToSpec.js
+++ test/spec/validateMustEqualToSpec.js
@@ -29,35 +29,61 @@ describe('Directive: Validate Must Equal To', function () {
     form.password.$setViewValue('12345678');
 
     expect($scope.password).toBe('12345678');
-    expect(form.password.$valid).toBe(true);
+    expect(form.password.$valid).toBeTruthy();
   });
 
-  it('should invalidate field when its value is not equal to the other field', function () {
-    form.password2.$untouched = false;
-
-    $scope.$apply(function () {
-      $scope.password2 = '87654321';
-    });
-
+  it('should validate when the other field\'s value is empty', function () {
     form.password.$setViewValue('12345678');
-
-    /*
-     * Because the validation did not succeed the value is unset on the ng-model
-     */
-    expect($scope.password).toBe(undefined);
-    expect(form.password.$valid).toBe(false);
-  });
-
-  it('should validate field when its value is equal to the other field', function () {
     form.password2.$untouched = false;
-    $scope.$apply(function () {
-      $scope.password2 = '12345678';
-    });
-
-    form.password.$setViewValue('12345678');
+    form.password2.$setViewValue('');
 
     expect($scope.password).toBe('12345678');
-    expect(form.password.$valid).toBe(true);
+    expect(form.password.$valid).toBeTruthy();
   });
 
+  describe('both values do not equal', function () {
+    beforeEach(function () {
+      form.password2.$untouched = false;
+
+      $scope.$apply(function () {
+        $scope.password2 = '87654321';
+      });
+    });
+
+    it('should invalidate field when its value is not equal to the other field', function () {
+      form.password.$setViewValue('12345678');
+
+      expect($scope.password).toBe(undefined);
+      expect(form.password.$invalid).toBeTruthy();
+    });
+
+    it('should invalidate the other field when the value is not equal', function () {
+      form.password.$setViewValue('12345678');
+
+      expect(form.password2.$invalid).toBeTruthy();
+    });
+  });
+
+  describe('both values are equal', function () {
+    beforeEach(function () {
+      form.password2.$untouched = false;
+
+      $scope.$apply(function () {
+        $scope.password2 = '12345678';
+      });
+    });
+
+    it('should validate field when its value is equal to the other field', function () {
+      form.password.$setViewValue('12345678');
+
+      expect($scope.password).toBe('12345678');
+      expect(form.password.$valid).toBeTruthy();
+    });
+
+    it('should invalidate the other field when the value is not equal', function () {
+      form.password.$setViewValue('12345678');
+
+      expect(form.password2.$valid).toBeTruthy();
+    });
+  });
 });
